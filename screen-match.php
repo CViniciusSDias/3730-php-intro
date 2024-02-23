779421fe6d823755d9e321feb8cb57f5964b1873
Vinicius Dias <carlosv775@gmail.com>
2024-02-23 00:12:16 -0300

    Projeto final do curso

---
+++ screen-match.php
@@ -0,0 +1,50 @@
+<?php
+
+echo "Bem-vindo(a) ao screen match!\n";
+
+$nomeFilme = "Top Gun - Maverick";
+
+$anoLancamento = 2022;
+
+$quantidadeDeNotas = $argc - 1;
+$notas = [];
+
+for ($contador = 1; $contador < $argc; $contador++) {
+    $notas[] = (float) $argv[$contador];
+}
+
+$notaFilme = array_sum($notas) / $quantidadeDeNotas;
+$planoPrime = true;
+
+$incluidoNoPlano = $planoPrime || $anoLancamento < 2020;
+
+echo "Nome do filme: " . $nomeFilme . "\n";
+echo "Nota do filme: $notaFilme\n";
+echo "Ano de lançamento: $anoLancamento\n";
+
+if ($anoLancamento > 2022) {
+    echo "Esse filme é um lançamento\n";
+} elseif($anoLancamento > 2020 && $anoLancamento <= 2022) {
+    echo "Esse filme ainda é novo\n";
+} else {
+    echo "Esse filme não é um lançamento\n";
+}
+
+$genero = match ($nomeFilme) {
+    "Top Gun - Maverick" => "ação",
+    "Thor: Ragnarok" => "super-herói",
+    "Se beber não case" => "comédia",
+    default => "gênero desconhecido",
+};
+
+echo "O gênero do filme é: $genero\n";
+
+$filme = [
+    "nome" => "Thor: Ragnarok",
+    "ano" => 2021,
+    "nota" => 7.8,
+    "genero" => "super-herói",
+];
+
+
+echo $filme["ano"];
