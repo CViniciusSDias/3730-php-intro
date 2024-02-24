324a56f19f30d71b93da06a4d397143eee5c35dd
Vinicius Dias <carlosv775@gmail.com>
2024-02-24 20:44:28 -0300

    Video 2.3

---
+++ screen-match/funcoes.php
@@ -0,0 +1,15 @@
+<?php
+
+function exibeMensagemLancamento(int $ano): void {
+    if ($ano > 2022) {
+        echo "Esse filme é um lançamento\n";
+    } elseif($ano > 2020 && $ano <= 2022) {
+        echo "Esse filme ainda é novo\n";
+    } else {
+        echo "Esse filme não é um lançamento\n";
+    }
+}
+
+function incluidoNoPlano(bool $planoPrime, int $anoLancamento): bool {
+    return $planoPrime || $anoLancamento < 2020;
+}
--- screen-match/screen-match.php
+++ screen-match/screen-match.php
@@ -1,18 +1,6 @@
 <?php
 
-function exibeMensagemLancamento(int $ano): void {
-    if ($ano > 2022) {
-        echo "Esse filme é um lançamento\n";
-    } elseif($ano > 2020 && $ano <= 2022) {
-        echo "Esse filme ainda é novo\n";
-    } else {
-        echo "Esse filme não é um lançamento\n";
-    }
-}
-
-function incluidoNoPlano(bool $planoPrime, int $anoLancamento): bool {
-    return $planoPrime || $anoLancamento < 2020;
-}
+require __DIR__ . "/funcoes.php";
 
 echo "Bem-vindo(a) ao screen match!\n";
 
