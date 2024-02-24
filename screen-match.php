e132704f1ce46912eadd94c476916a90691c426d
Vinicius Dias <carlosv775@gmail.com>
2024-02-24 20:31:46 -0300

    Video 2.2

--- screen-match.php
+++ screen-match.php
@@ -61,4 +61,10 @@ var_dump($notas);
 sort($notas);
 var_dump($notas);
 $menorNota = min($notas);
-echo $menorNota;
+var_dump($menorNota);
+
+var_dump($filme['nome']);
+$posicaoDoisPontos = strpos($filme['nome'], ':');
+var_dump($posicaoDoisPontos);
+
+var_dump(substr($filme['nome'], 0, $posicaoDoisPontos));
