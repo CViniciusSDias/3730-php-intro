b79c6d66bed6fbdfc86030d1485bba7e14ee155f
Vinicius Dias <carlosv775@gmail.com>
2024-02-24 20:01:14 -0300

    Video 1.2

--- screen-match.php
+++ screen-match.php
@@ -1,5 +1,15 @@
 <?php
 
+function exibeMensagemLancamento($ano) {
+    if ($ano > 2022) {
+        echo "Esse filme é um lançamento\n";
+    } elseif($ano > 2020 && $ano <= 2022) {
+        echo "Esse filme ainda é novo\n";
+    } else {
+        echo "Esse filme não é um lançamento\n";
+    }
+}
+
 echo "Bem-vindo(a) ao screen match!\n";
 
 $nomeFilme = "Top Gun - Maverick";
@@ -22,13 +32,7 @@ echo "Nome do filme: " . $nomeFilme . "\n";
 echo "Nota do filme: $notaFilme\n";
 echo "Ano de lançamento: $anoLancamento\n";
 
-if ($anoLancamento > 2022) {
-    echo "Esse filme é um lançamento\n";
-} elseif($anoLancamento > 2020 && $anoLancamento <= 2022) {
-    echo "Esse filme ainda é novo\n";
-} else {
-    echo "Esse filme não é um lançamento\n";
-}
+exibeMensagemLancamento($anoLancamento);
 
 $genero = match ($nomeFilme) {
     "Top Gun - Maverick" => "ação",
