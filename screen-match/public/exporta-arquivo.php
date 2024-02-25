4f8df1142c61b90286a575d21bcc3a45a2733a01
Vinicius Dias <carlosv775@gmail.com>
2024-02-24 22:06:21 -0300

    Video 5.3

--- screen-match/public/exporta-arquivo.php
+++ screen-match/public/exporta-arquivo.php
@@ -8,3 +8,5 @@ $filme = [
 ];
 
 file_put_contents('filme.json', json_encode($filme));
+
+header('Location: /sucesso.php?filme=' . $filme['nome']);
---
+++ screen-match/public/sucesso.php
@@ -0,0 +1,1 @@
+<h1>Sucesso! Filme <?php echo $_GET['filme']; ?> inserido.</h1>
