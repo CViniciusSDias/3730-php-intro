98c4d4fdbdaa314545a29ec5d8de11d854f25741
Vinicius Dias <carlosv775@gmail.com>
2024-03-16 16:19:42 -0300

    Video 2.1

--- screen-match/src/Modelo/Filme.php
+++ screen-match/src/Modelo/Filme.php
@@ -4,5 +4,18 @@ class Filme {
     public string $nome;
     public int $anoLancamento;
     public string $genero;
-    public float $nota;
+    public array $notas = [];
+
+    public function avalia(float $nota): void
+    {
+        $this->notas[] = $nota;
+    }
+
+    public function media(): float
+    {
+        $somaNotas = array_sum($this->notas);
+        $quantidadeNotas = count($this->notas);
+
+        return $somaNotas / $quantidadeNotas;
+    }
 }
